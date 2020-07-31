b3de5fb8a263cd16c38a96dc111db5fd721ffbbe
1000384137sofia <bsgalvis73@misena.edu.cogit config --global user.email bsgalvis73@misena.edu.co git config --global user.name 100038413>
2020-07-31 16:47:07 -0500

    creado primer modelo:Artista, se cargo la base de datos al proyecto

---
+++ app/Artista.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Artista extends Model
+{
+    protected $table = "artists";
+    protected $primaryKey = "ArtistId";
+    public $timestamps = false;
+
+}
